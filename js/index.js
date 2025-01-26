96c056b47c2fb89d1494f9de1f353c2736f81504
EduardovJS <eduardoviniciusjorge@gmail.com>
2025-01-26 19:22:32 -0300

    As quatro formas de saida no javascript

--- js/index.js
+++ js/index.js
@@ -1,1 +1,4 @@
-document.getElementById("Teste").innerHTML="Meu primeiro texto <b>JavaScript<b>"; // Passa informações javascript para o html, pegando o Id do elemento e subscrevendo ele usando o innerHTML
+document.getElementById("Titulo").innerHTML="Meu primeiro texto <b>JavaScript<b>"; // Passa informações javascript para o html, pegando o Id do elemento e subscrevendo ele usando o innerHTML
+console.log("Isso é um alerta no console"); // Apresenta informação no terminal.
+alert("Isso é um alerta"); // Outra forma de apresentar informação na tela, uma caixa de alerta
+alert(5 + 5)
---
+++ js/resumo.md
@@ -0,0 +1,6 @@
+As quatro formas de saída no JavaScript: 
+
+.innerHtml  - Passa informações JavaScript para o HTML.
+console.log() - Passa informações JavaScript para o console do navegador.
+alert() - Exibe uma caixa de alerta para o navegador.
+document.write - Usando o document.write dentro da tag <script></script> pode escrever diretamente no HTML e pode ser ultilizado tags que serão aplicadas.
