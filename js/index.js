bbfff8c89566e01157db98d3ac877a81a8bb59df
EduardovJS <eduardoviniciusjorge@gmail.com>
2025-01-26 18:57:10 -0300

    primeiro commit

---
+++ js/index.js
@@ -0,0 +1,1 @@
+document.getElementById("Teste").innerHTML="Meu primeiro texto <b>JavaScript<b>"; // Passa informações javascript para o html, pegando o Id do elemento e subscrevendo ele usando o innerHTML
