020d72a891f7ef061db7ca6ac5657c446fead676
EduardovJS <eduardoviniciusjorge@gmail.com>
2025-01-27 22:00:38 -0300

    var, let const

--- js/index.js
+++ js/index.js
@@ -2,3 +2,34 @@ document.getElementById("Titulo").innerHTML="Meu primeiro texto <b>JavaScript<b>
 console.log("Isso é um alerta no console"); // Apresenta informação no terminal.
 alert("Isso é um alerta"); // Outra forma de apresentar informação na tela, uma caixa de alerta
 alert(5 + 5)
+
+// Entendo os três tipos de variaveis no JavaScript
+
+// -> var 
+// -> let 
+// -> const
+
+//-> var
+//Funciona como um var normal.
+var coisa = "Teste String"  // 
+var a = 2; 
+var b = 3; 
+var c = 3 + 5;
+alert(c)
+
+//-> let
+//Não permite redeclaração de uma variavel, somente dentro de um bloco. 
+//Se usa o let em vez de var geralmente. 
+let pessoa = "Pessoa";
+// let pessoa = "Pessoa1"; // Não permite
+{
+    let pessoa = "Pessoa3"; // A variavel mudou, pois está dentro de um bloco.
+    alert(pessoa)
+}
+
+
+
+//-> const
+//O valor da variável declarada sempre será o mesmo, não aceita mudanças.
+//Quando uma variável precisa ter um valor que não pode ser mudado.
+const x = 2;
