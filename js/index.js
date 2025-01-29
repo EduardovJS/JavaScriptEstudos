c3acfca93fbedd2580b8661b945c2685effa9eeb
EduardovJS <eduardoviniciusjorge@gmail.com>
2025-01-28 22:47:55 -0300

    operadores aritmetricos e operador ternario

--- js/index.js
+++ js/index.js
@@ -3,33 +3,32 @@ console.log("Isso é um alerta no console"); // Apresenta informação no termin
 alert("Isso é um alerta"); // Outra forma de apresentar informação na tela, uma caixa de alerta
 alert(5 + 5)
 
-// Entendo os três tipos de variaveis no JavaScript
+// Entendo os operadores em JavaScript 
 
-// -> var 
-// -> let 
-// -> const
+// São esses os operadores : + - * / = ++ -- += && || etc.. 
 
-//-> var
-//Funciona como um var normal.
-var coisa = "Teste String"  // 
-var a = 2; 
-var b = 3; 
-var c = 3 + 5;
-alert(c)
+// Para verificar se é o VALOR é o mesmo.
+var valor1, valor2
+valor1 = 5;
+valor2 = "5"; 
+total = (valor1 == valor2) 
+alert(total) // Vai dar true, pois o valor é mesmo, não é comparado o TIPO. 
 
-//-> let
-//Não permite redeclaração de uma variavel, somente dentro de um bloco. 
-//Se usa o let em vez de var geralmente. 
-let pessoa = "Pessoa";
-// let pessoa = "Pessoa1"; // Não permite
-{
-    let pessoa = "Pessoa3"; // A variavel mudou, pois está dentro de um bloco.
-    alert(pessoa)
-}
+//Para verificar se é o VALOR é o TIPO é o mesmo. 
+valor1 = 5;
+valor2 = "5";
+total = (valor1 === valor2)
+alert(total) // Vai dar false, pois o valor é mesmo, porém o TIPO é diferente.
+
+valor1 = 5;
+valor2 = "5";
+total = (valor1 !== valor2)
+alert(total) // Vai dar true, pois o valor é mesmo, porém o TIPO é diferente.
 
 
-
-//-> const
-//O valor da variável declarada sempre será o mesmo, não aceita mudanças.
-//Quando uma variável precisa ter um valor que não pode ser mudado.
-const x = 2;
+// Operador Ternário
+var idade, eleitor;
+idade = 18;
+// Se nao for eleito faz a primeira condição, se não faz a segunda condição.
+eleito = (idade < 18) ? "Não, é eleito" : "Sim, é eleitor";
+alert("Resposta: " + eleito + " idade é de " + idade);
