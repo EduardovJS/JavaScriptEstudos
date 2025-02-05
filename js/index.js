2ee06448a9927027051e99074a4bd1855258e96a
EduardovJS <eduardoviniciusjorge@gmail.com>
2025-02-04 21:32:35 -0300

    json

--- js/index.js
+++ js/index.js
@@ -1,76 +1,20 @@
-class Carro{
-    constructor(valor1, valor2, valor3){
-        this.marca = valor1;
-        this.modelo = valor2;
-        this.ano = valor3;
-    }
+// JSON
+// Converte objetos em texto ou textos em objetos.
+
+// JSON.parse() -> Converte texto no padrão JSON em objetos.
+// JSON.stringify() -> Converte objetos em textos padrão JSON.
+
+const carro = {
+    marca: "Fiat",
+    modelo: "Uno",
+    ano: "2001"
 }
+// Mostra o objeto, não mostra o conteudo
+document.getElementById('area').innerHTML = carro;
 
-const uno = new Carro("Fiat", "Uno", 2001);
-const gol = new Carro("Volkswagen", "Gol", 2013);
-
-console.log(uno);
-console.log(gol);
-
-// pega a data completa
-let data = new Date();
-console.log(data);
-
-// pega o ano completo
-let ano = data.getFullYear(); 
-console.log(ano);
-
-// pega o mês atual - de 0 até 11 sendo 0 janeiro e 11 dezembro
-let mes = data.getMonth();
-console.log(mes);
-
-// pega o dia do mês - 1 até 31 
-let diaMes = data.getDate();
-console.log(diaMes);
-
-// pega o dia da semana - 1 até 6
-let diaSemana = data.getDay();
-console.log(diaSemana);
-
-// pega as horas de - 0 até 23
-let hora = data.getHours();
-console.log(hora);
-
-// pega as horas de - 0 até 59
-let minutos = data.getMinutes();
-console.log(minutos);
-
-// pega segundos - 0 até 59
-let segundos = data.getSeconds();
-console.log(segundos);
-
-// pega milisegundos = 0 até 999
-let milisegundos = data.getMilliseconds();
-console.log(milisegundos);
-
-// pega a data do padrão brasileiro - dia/mes/ano
-let dataBR = data.toLocaleDateString('pt-BR');
-console.log(dataBR);
-
-// Comparar datas - maior ou menor 
-
-var hoje = new Date();
-var vencimento = new Date(2025, 2, 7);
-
-if(hoje > vencimento){
-    console.log("Sua conta está vencida");
-} else{
-    console.log("Ainda não venceu");
-}
-
-// Diferença entre duas datas em dias 
-var dataInicial = new Date();
-var dataFinal = new Date(2025, 2, 11);
-// milisegundos
-var diferencaTempo = dataFinal.getTime() - dataInicial.getTime();
-// converte para dias
-var diferencaDias = Math.ceil(diferencaTempo / (24*60*60*1000));
-
-console.log(diferencaDias + " dias");
-
-
+// Mostra o conteudo do objeto
+let texto = JSON.stringify(carro);
+document.getElementById('area1').innerHTML = texto;
+// Converte para objeto
+let obj = JSON.parse(texto)
+console.log(obj.modelo)
